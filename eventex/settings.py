de8ea9296ebe6abf579247bb76ddb29650266dc4
andressa.lopes <andressa.lopes@instruct.com.br>
2019-11-05 14:29:23 -0300

    Criando e configurando Admin

--- eventex/settings.py
+++ eventex/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'test_without_migrations',
     'django_extensions',
     'eventex.core',
-    'eventex.subscriptions',
+    'eventex.subscriptions.apps.SubscriptionsConfig',
 ]
 
 MIDDLEWARE = [
