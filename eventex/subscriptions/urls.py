e5c93f055da750defac2a7c83d91f55bb6759c61
andressa.lopes <andressa.lopes@instruct.com.br>
2019-11-06 16:32:25 -0300

    Extrai urls hardcoded

---
+++ eventex/subscriptions/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from eventex.subscriptions.views import new, detail
+
+app_name = 'subscriptions'
+
+urlpatterns = [
+
+    path('', new, name='new'),
+    path('inscricao/<int:pk>/', detail, name='detail'),
+
+]
