f69263fbccef4f1fa9c15170e417b7cb26b7a24a
andressa.lopes <andressa.lopes@instruct.com.br>
2019-11-14 15:25:54 -0300

    Coloca email e telefone opcionais nas inscrições

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -11,8 +11,8 @@ def validate_cpf(value):
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
     cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-    email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Telefone')
+    email = forms.EmailField(label='Email', required=False)
+    phone = forms.CharField(label='Telefone', required=False)
 
     def clean_name(self):
         name = self.cleaned_data['name']
@@ -22,3 +22,8 @@ class SubscriptionForm(forms.Form):
 
         capitalized_name = ' '.join(words)
         return capitalized_name
+
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou telefone.')
+        return self.cleaned_data
